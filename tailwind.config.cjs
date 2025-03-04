6a1f23e26f3ee50748546d9e2be73fee466e38c0
rwaddinsall <r.addinsall@gmail,com>
2025-03-04 23:13:13 +1100

    Landing layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+      extend: {
+        colors: {
+          site: {
+            background: '#fef6e4',
+            headline: '#001858',
+            paragraph: '#172c66',
+            accent: '#f582ae',
+          }
+        },
       },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+    plugins: [require("@tailwindcss/typography")],
+  };
